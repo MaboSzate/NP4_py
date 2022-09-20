126c6708548a8687c7b6ad976944618cefb06fca
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2022-09-20 10:35:36 +0200

    kesz

--- benzintartály mérete/main.py
+++ benzintartály mérete/main.py
@@ -1,4 +1,15 @@
+hol_volt = 0
+mm = -1
+
 with open("be1.txt") as f:
     K, N, B, L=[int(i) for i in f.readline().split(" ")]
-print(K, N, B, L)
+    for i in range(N):
+        hol, mennyit = [int(i) for i in f.readline().split(" ")]
+        B = B - (hol-hol_volt)/100 * L + mennyit
+        if B > mm:
+            mm = B
+        hol_volt = hol
+
+print(mm)
+
 
