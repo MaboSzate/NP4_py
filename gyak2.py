3f6d7ea872d5006f6fb621439d42de87cb771b1e
MaboSzate <90611692+MaboSzate@users.noreply.github.com>
2022-09-20 08:51:31 +0200

    Add files via upload

---
+++ gyak2.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+r_eff = np.array([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
+IC = np.exp(r_eff)
+#print(type(a))
+print(IC)
+
+IC_2y = IC**2
+r_2y = np.log(IC_2y)
+print(r_2y)
+
+print(r_eff.shape)
+
+
+a_elso = np.array([[1, 2], [2, 5], [2, 8]])
+print(np.sum(a_elso))
+print(np.sum(a_elso, axis=0))
+print(np.sum(a_elso, axis=1))
+
+print(np.mean(a_elso, axis=0))
+print(np.std(a_elso, axis=0))
+print(np.min(a_elso, axis=0))
+print(np.max(a_elso, axis=0))
+print(np.diff(a_elso, axis=0))
+
+c = a_elso - np.mean(a_elso)
+print(c)
+print(np.mean(c))
+print(c.mean())
+
+is_pos = c>0
+print(is_pos)
+
+d = c.copy()
+
+d[is_pos] += 100
+print(d)
+print(c)
+
+a_new = np.array([[2, 2], [2, 4], [0, 4]])
+print(a_new.std(axis=1))
+print(a_new.std(axis=1, ddof=0))
+print(a_new.std(axis=1, ddof=1))
+
+#random numbers
