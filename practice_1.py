6a265d3dbd57c8964b61573ab01067de8a669327
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2022-09-27 09:19:28 +0200

    practicing again

--- practice_1.py
+++ practice_1.py
@@ -1,15 +1,35 @@
-# function (r_exp, vols, numOfPath)
-# numofpath sor
 import numpy as np
-
-rs = np.array([0.1, 0.05])
-vol = np.array([0.2, 0.01])
+import seaborn as sns
 
 
-def returns(r_exp, vols, numOfPath):
+def uncorr_returns(r_exp, vols, numOfPath):
     n = len(r_exp)
-    yields = np.random.normal(r_exp, vols, [numOfPath, n])
-    return yields
+    r_exp = np.array(r_exp)
+    vols = np.array(vols)
+    alfa = r_exp - vols * vols / 2
+    returns = np.random.normal(alfa, vols, [numOfPath, n])
+    return returns
 
 
-print(returns(rs, vol, 10))
+def test_uncorr_reutrns():
+    rs = [0.1, 0.05]
+    vol = [0.2, 0.01]
+    print(uncorr_returns(rs, vol, 10))
+#test_uncorr_reutrns()
+
+
+def corr_std_norm(corrMat, numOfPath):
+    a_corr = np.array(corrMat)
+    numOfAssets = a_corr.shape[0]
+    a_l = np.linalg.cholesky(a_corr)
+    a_uncorr = np.random.normal(size=(numOfPath, numOfAssets))
+    z = (np.matmul(a_l, a_l.transpose()))
+
+    pass
+
+
+def test_corr_std_norm():
+    corr_mat = [[1.0, -.8], [-.8, 1.0]]
+    print(corr_std_norm(corr_mat, 1000))
+
+test_corr_std_norm()
