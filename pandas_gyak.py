9caf7ea574136835049aed9008a73312a9afc3ad
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2022-10-04 11:21:04 +0200

    vege

--- pandas_gyak.py
+++ pandas_gyak.py
@@ -31,9 +31,47 @@ df = pd.DataFrame(data={'A': ['F', 4, 'S'],
 
 df_voo['Volume in 1000s'] = df_voo['Volume'] / 1000
 df_voo['Open Close Diff'] = df_voo['Open'] - df_voo['Close']
-#print(df_voo)
+# print(df_voo)
 
 df_merged = df_voo.merge(df_tlt, how='inner', on='Date', suffixes=('_voo', '_tlt'))
 print(df_merged)
 
 df_merged_filtered = df_merged[['Date', 'Adj Close_voo', 'Adj Close_tlt']]
+
+df_prop = pd.read_csv('property data.csv')
+df_prop_berkeley = df_prop.loc[df_prop['ST_NAME'] == 'BERKELEY']
+msk = df_prop['ST_NAME'].isin(['BERKELEY', 'LEXINGTON'])
+df_prop_berlex = df_prop.loc[msk,]
+print(msk)
+
+msk2 = df_prop['ST_NUM'] < 200
+df_prop_smallNum = df_prop.loc[msk2]
+msk3 = (df_prop['ST_NUM'] < 200) & (df_prop['ST_NAME'] == 'LEXINGTON')
+df_prop_smallNumLex = df_prop.loc[msk3]
+
+mskNAN = df_prop['ST_NUM'].notnull()
+df_prop["PID"] = df_prop["PID"].fillna(float('nan'))
+
+df_prop = df_prop.loc[mskNAN]
+# print(df_prop)
+
+df_prop_copy = df_prop.copy()
+for col in df_prop_copy.columns:
+    df_prop_copy[col] = df_prop_copy[col].fillna("NEM JÓ")
+
+# print(df_prop_copy)
+
+df_prop_copy2 = df_prop.copy()
+df_prop_copy2 = df_prop_copy2.fillna("FOS")
+
+# print(df_prop_copy2)
+
+df_voo['r_eff'] = df_voo['Adj Close'] / df_voo['Adj Close'].shift(1) - 1
+print(df_voo[['Adj Close', 'r_eff']])
+
+import numpy as np
+
+df_voo['log_return'] = np.log(df_voo['Adj Close'] / df_voo['Adj Close'].shift(1))
+df_voo['cum_log_return'] = df_voo['log_return'].cumsum()
+print(df_voo)
+
