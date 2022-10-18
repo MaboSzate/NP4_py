0666576d53ea9025607f5c18918ef373c9763028
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2022-10-18 09:30:59 +0200

    class added

--- iteracio.py
+++ iteracio.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+class VelSzamok:
+
+    def __init__(self, dim1, dim2):
+        self.dim1 = dim1
+        self.dim2 = dim2
+        self.value = np.random.random([dim1, dim2])
+
+    def atlag(self):
+        atlagok = np.mean(self.value, axis=0)
+        plt.scatter([range(self.dim2)], atlagok)
+        plt.show()
+
 
 df = pd.DataFrame(range(1, 21), columns=['n'])
 df = df.set_index('n')
@@ -14,3 +29,7 @@ for idx, row in df.iterrows():
         df.loc[idx, 'fib'] = df.loc[idx-1, 'fib'] + df.loc[idx-2, 'fib']
 
 print(df)
+
+matrix = VelSzamok(2, 3)
+print(matrix.value)
+matrix.atlag()
