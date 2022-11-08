874dd142746d72a6959446de790668debf65276c
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2022-11-08 11:22:42 +0100

    8. ora vege

--- GBM.py
+++ GBM.py
@@ -2,18 +2,19 @@ from GBM_class import GBPath
 import matplotlib.pyplot as plt
 import numpy as np
 from opciok_class import Option
+import pandas as pd
 
 gb = GBPath()
 
 sigma=0.35
 N=250
-S0=1000
-spots = gb.generate(sigma,0,S0,N,1)
+K=1000
+spots = gb.generate(sigma,0,K,N,1)
 times = np.arange(0,1,1/N)
 #plt.plot(times,spots)
 # plt.show()
 
-opt = Option("C", 1, S0, None)
+opt = Option("C", 1, K, None)
 
 vola = 0.3
 prices = []
@@ -26,3 +27,16 @@ for (t,S) in zip(times, spots):
 
 plt.plot(times, np.array(prices))
 plt.show()
+
+df = pd.DataFrame({"time": times, "spot": spots})
+
+
+def calc_price(row):
+    opt = Option("C", 1, K, None)
+    vola = 0.3
+    return opt.calc_price(row.spot, 1-row.time, vola)
+
+
+df["price"] = df.apply(calc_price, axis=1)
+df.price.plot()
+#plt.show()
